f8ba7c9d79c7e9378b270de119dacdd5d72d46ae
DiegoHaczek <diegohaczek1996@hotmail.com>
2022-11-12 00:06:17 -0300

    html email

--- Controllers/CuponController.php
+++ Controllers/CuponController.php
@@ -69,8 +69,27 @@
                 $mail->setFrom('heropet3@gmail.com', 'Pet Hero');
                 $mail->addAddress($mailUsuario);
                 $mail->Subject = 'Cupon de Pago - Pet Hero';
-                $mail->Body = 'Aqui esta el link de pago: 
-                http://localhost/TP-FINAL-PET-HERO/cupon/verCupon?id_cupon='.$idCupon.' ';
+                /*$mail->Body = 'Aqui esta el link de pago: 
+                http://localhost/TP-FINAL-PET-HERO/cupon/verCupon?id_cupon='.$idCupon.' ';*/
+                $mail->Body = '<div style="display:flex;flex-direction:column;align-items:center;width:600px;height:450px;background-color:rgb(231, 231, 179);">
+
+
+
+                <div style="margin-top:3.5em;position:inherit;display:flex;flex-direction: column;justify-content: space-around;width:65%;height:300px;background-color: rgb(245, 255, 240);line-height: 1.6rem;border-radius:5px;">
+                
+                <h2 style="font-family:Arial;color:#054605;margin-left: 1.3em;margin-bottom: -0.5em;">NOMBRE, Tu Cupon de Pago Esta Listo</h2>
+        
+                <span style="font-family:arial;margin-left: 4.75em;letter-spacing: 1px;width:65%">Haz click en el boton inferior para acceder a tu cupon de pago y confirmar tu reserva </span>
+                
+                <button style="width:30%;height:11%;align-self:center;margin-bottom: 1em;cursor:pointer;background-color:#628b33;border:none;border-radius:6px;color:white;">
+                <a href="http://localhost/TP-FINAL-PET-HERO/cupon/verCupon?id_cupon='.$idCupon.'">Ver Cupon</a></button>
+        
+                </div>
+        
+                <span style="margin-top:3em;">Pet Heroe - 2022</span>
+        
+                </div>';
+                $mail->IsHTML(true);
                 $mail->send();
                 //echo 'Se mandó el mensaje';
                 
